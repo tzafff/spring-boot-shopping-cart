20ee57115941450d6f5b2343f21513a2bd4979ff
tzafff <christzaf99@gmail.com>
2025-01-23 15:57:17 +0200

    Image Service Implementation and Dto for images.

---
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/dto/ImageDto.java
@@ -0,0 +1,10 @@
+package com.ctzaf.dreamshops.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
+}
---
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.ctzaf.dreamshops.repository;
+
+import com.ctzaf.dreamshops.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
