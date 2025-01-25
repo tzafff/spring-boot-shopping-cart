c65bccd919792f1de0186c34ee821fa41dbf0bb5
tzafff <christzaf99@gmail.com>
2025-01-25 19:14:01 +0200

    CartItem Service Implementation

--- dream-shops/src/main/java/com/ctzaf/dreamshops/service/cart/CartItemService.java
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/service/cart/CartItemService.java
@@ -1,4 +1,87 @@
 package com.ctzaf.dreamshops.service.cart;
 
-public class CartItemService {
+import com.ctzaf.dreamshops.exceptions.ResourceNotFoundException;
+import com.ctzaf.dreamshops.model.Cart;
+import com.ctzaf.dreamshops.model.CartItem;
+import com.ctzaf.dreamshops.model.Product;
+import com.ctzaf.dreamshops.repository.CartItemRepository;
+import com.ctzaf.dreamshops.repository.CartRepository;
+import com.ctzaf.dreamshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService {
+
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final IProductService productService;
+    private final ICartService cartService;
+
+    @Override
+    public void addItemToCart(Long cartId, Long productId, int quantity) {
+        // 1. Get the cart
+        // 2. Get the product
+        // 3. Check if the product already in the cart
+        // 4. If it is, then increase the quantity with the requested quantity
+        // 5. If not, then initiate a new CartItem entry.
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+        CartItem cartItem = cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElse(new CartItem());
+        if(cartItem.getId() == null) {
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        } else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+
+        cart.removeItem(itemToRemove);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void updateQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getItems()
+                .stream()
+                .filter(item -> item.getCart().getId().equals(cartId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setQuantity(quantity);
+                    item.setUnitPrice(item.getProduct().getPrice());
+                    item.setTotalPrice();
+                });
+
+        BigDecimal totalAmount = cart.getTotalAmount();
+        cart.setTotalAmount(totalAmount);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        return cart.getItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Item Not Found!"));
+    }
 }
--- dream-shops/src/main/java/com/ctzaf/dreamshops/service/cart/ICartItemService.java
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/service/cart/ICartItemService.java
@@ -1,4 +1,11 @@
 package com.ctzaf.dreamshops.service.cart;
 
+import com.ctzaf.dreamshops.model.CartItem;
+
 public interface ICartItemService {
+    void addItemToCart(Long cartId,Long productId, int quantity);
+    void removeItemFromCart(Long cartId,Long productId);
+    void updateQuantity(Long cartId,Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
 }
