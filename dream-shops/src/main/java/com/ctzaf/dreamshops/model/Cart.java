9fe478e3a869cd39343a28753fa6bd88ec660ac6
tzafff <christzaf99@gmail.com>
2025-01-25 17:10:59 +0200

    Add Cart and CartItem Entities

---
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/model/Cart.java
@@ -0,0 +1,26 @@
+package com.ctzaf.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount = BigDecimal.ZERO;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true) // When a cart is deleted all the cart items will be deleted.
+    private Set<CartItem> cartItems;
+}
