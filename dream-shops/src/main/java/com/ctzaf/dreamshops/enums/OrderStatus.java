f903be3468235db3137c61c47430d4682bbdd30d
tzafff <christzaf99@gmail.com>
2025-02-05 14:52:25 +0200

    Add Order and OrderItem Models and New Enums for OrderStatus

---
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.ctzaf.dreamshops.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/model/Order.java
@@ -0,0 +1,29 @@
+package com.ctzaf.dreamshops.model;
+
+import com.ctzaf.dreamshops.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+}
