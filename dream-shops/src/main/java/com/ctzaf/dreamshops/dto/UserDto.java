b1fbd3f3cd554338661449f63ada8ceb49e0f61e
tzafff <christzaf99@gmail.com>
2025-02-06 21:00:41 +0200

    Add CartDto, CartItemDto, UserDto

---
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.ctzaf.dreamshops.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<OrderDto> orders;
+    private CartDto cart;
+
+}
