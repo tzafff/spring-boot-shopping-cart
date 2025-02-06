0514ead0084ed47998dd14e936ca9298a1427f9d
tzafff <christzaf99@gmail.com>
2025-02-06 21:05:20 +0200

    Implement convert User to UserDto method and return to frontend the UserDto

--- dream-shops/src/main/java/com/ctzaf/dreamshops/service/user/IUserService.java
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/service/user/IUserService.java
@@ -1,5 +1,6 @@
 package com.ctzaf.dreamshops.service.user;
 
+import com.ctzaf.dreamshops.dto.UserDto;
 import com.ctzaf.dreamshops.model.User;
 import com.ctzaf.dreamshops.request.CreateUserRequest;
 import com.ctzaf.dreamshops.request.UserUpdateRequest;
@@ -11,4 +12,5 @@ public interface IUserService {
     User updateUser(UserUpdateRequest request, Long userId);
     void deleteUser(Long userId);
 
+    UserDto convertUserToDto(User user);
 }
