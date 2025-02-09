7cdb93f9d9d7b4e1f647496dff0acc6ae1e197e4
tzafff <christzaf99@gmail.com>
2025-02-09 18:36:47 +0200

    Add Autowired on AuthTokenFilter

--- dream-shops/src/main/java/com/ctzaf/dreamshops/security/jwt/AuthTokenFilter.java
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/security/jwt/AuthTokenFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,7 +20,9 @@ import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
 
+    @Autowired
     private JwtUtils jwtUtils;
+    @Autowired
     private ShopUserDetailsService userDetailsService;
 
     @Override
