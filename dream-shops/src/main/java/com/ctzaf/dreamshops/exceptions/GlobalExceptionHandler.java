93e8c0294a74b7c44e7d976049a78be7d0b36d38
tzafff <christzaf99@gmail.com>
2025-02-09 18:45:34 +0200

    Add GlobalExceptionHandler for AccessDenied Exception

---
+++ dream-shops/src/main/java/com/ctzaf/dreamshops/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.ctzaf.dreamshops.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
+        String message = "You do not have permission to perform this action.";
+        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+    }
+}
